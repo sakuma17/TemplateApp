aa63059941e102bb2b9b7111387ba29ba427ecd1
Ntakeda <muzat.yakuzajeep@gmail.com>
2020-12-05 11:54:01 +0900

    武田自己紹介

---
+++ Takeda.java
@@ -0,0 +1,11 @@
+public class Takeda extends Template{
+	public Takeda(String name){
+		super(name);
+	}
+	@Override
+	public void content(){
+		System.out.println("夫婦と猫で3人暮らしです");
+		System.out.printf("最近妻が何故かプロレスにはまり、%nグレートムタの真似をして毒霧を吐いてきます。%n");
+		System.out.println("ちょっと困ります");
+	}
+}
