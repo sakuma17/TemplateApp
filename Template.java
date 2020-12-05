5267792094976624ffe5e0bc2938c8e208e358f0
mjpurin <mjpurin@gmail.com>
2020-12-05 11:35:28 +0900

    Template.java追加

---
+++ Template.java
@@ -0,0 +1,19 @@
+public abstract class Template{
+	String name;
+	public Template(String name){
+		this.name=name;
+	}
+	public void intro(){
+		System.out.println("はじめまして、"+this.name+"です!");
+	}
+	public abstract void content();
+	public void ending(){
+		System.out.println("よろしくおねがいします");
+	}
+	public final void introduce(){
+		intro();
+		content();
+		ending();
+	}
+}
+
