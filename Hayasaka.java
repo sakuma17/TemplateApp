b3d565de32a4f0edc5f3bd1fc2c5795ad114db8e
Mieko Hayasaka <hayasaka654@gmail.com>
2020-12-05 11:52:23 +0900

    Hayasaka.java追加

---
+++ Hayasaka.java
@@ -0,0 +1,9 @@
+public class Hayasaka extends Template{
+	public Hayasaka(String name){
+		super(name);
+	}
+	@Override
+	public void content(){
+		System.out.println("趣味はビールを飲みながらTVドラマを見ることです");
+	}
+}
