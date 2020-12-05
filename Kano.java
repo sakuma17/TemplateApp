f2d1a2cd818bfd78c3d9fae921d1263baa67f070
kyoko <edgwcnn4869@gmail.com>
2020-12-05 11:59:29 +0900

    kano追加

---
+++ Kano.java
@@ -0,0 +1,11 @@
+public class Kano extends Template{
+	public Kano(String name){
+		super(name);
+	}
+	@Override
+	public void content(){
+		System.out.println("出身は神奈川です。現在は千葉在住です。");
+		System.out.println("趣味はゲーム、読書、ミニチュア収集です。");
+		Ststem.out.println("最近ハマったゲームはUNDERTALEです。");
+	}
+}
