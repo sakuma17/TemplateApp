cf9a40e8a9d99843b029e64a4e246de8b93fefbd
sato <kyousukesato8@gmail.com>
2020-12-05 11:58:28 +0900

    Sato.java追加

---
+++ Sato.java
@@ -0,0 +1,10 @@
+public class Sato extends Template{
+	public Sato(String name){
+		super(name);
+	}
+	@Override
+	public void content(){
+		System.out.println("趣味は手芸です。");
+		System.out.println("レジン、ビーズ細工、ニードルフェルト等をやっています。");
+	}
+}
