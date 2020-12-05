1337028e3e4e306e35a775e56bf58e85fda08e45
sakuma17 <ids08663@gmail.com>
2020-12-05 12:01:18 +0900

    Sakuma

---
+++ Sakuma.java
@@ -0,0 +1,9 @@
+public class Sakuma extends Template{
+	public Sakuma(String name){
+		super(name);
+	}
+	@Override
+	public void content(){
+		System.out.println("趣味はゲームをすること、アニメを見ること、音楽を聴くこと、歌うことです。");
+	}
+}
